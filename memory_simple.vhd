cf4098c06ee53c875290b014284a5dceb3802138
Nolan Meske <nolanmeske@icloud.com>
2018-03-31 15:01:43 -0700

    reduced size of array to appease synthesis engine

--- memory_simple.vhd
+++ memory_simple.vhd
@@ -1,18 +1,18 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    09:47:39 03/26/2018 
--- Design Name: 
--- Module Name:    memory_simple - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    09:47:39 03/26/2018
+-- Design Name:
+-- Module Name:    memory_simple - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments: Being byte addressable doesn't mean that you should be able to
 -- write bytes, right?
@@ -32,7 +32,7 @@ use ieee.numeric_std.all;
 --use UNISIM.VComponents.all;
 
 entity memory_simple is
-port(		clk : in std_logic;
+port(	clk : in std_logic;
 			enable : in std_logic;
 			din : in std_logic_vector (15 downto 0);
 			adr : in std_logic_vector (15 downto 0);
@@ -41,34 +41,36 @@ port(		clk : in std_logic;
 end memory_simple;
 
 architecture Behavioral of memory_simple is
-	
-	type mem_array is array ((2**16 - 1) downto 0) of std_logic_vector (7 downto 0);
+
+	type mem_array is array (127 downto 0) of std_logic_vector (7 downto 0);
 	signal mem : mem_array;
 
 begin
 
 	mem_proc : process (clk)
-	
+
 		variable lsb_index : integer := 0;
 		variable msb_index : integer := 0;
-		
+
 	begin
-	
-		if rising_edge(clk) and enable = '1' then
-	
-			lsb_index := to_integer(unsigned(adr));
-			msb_index := lsb_index + 1;
-		
-			if mode = '1' then
-				mem(lsb_index) <= din (7 downto 0);
-				mem(msb_index) <= din (15 downto 8);
-			elsif mode = '0' then
-				dout <= mem(msb_index) & mem(lsb_index);
+
+		if rising_edge(clk) then
+			if enable = '1' then
+
+				lsb_index := to_integer(unsigned(adr));
+				msb_index := lsb_index + 1;
+
+				if mode = '1' then
+					mem(lsb_index) <= din (7 downto 0);
+					mem(msb_index) <= din (15 downto 8);
+				elsif mode = '0' then
+					dout <= mem(msb_index) & mem(lsb_index);
+				end if;
+
 			end if;
-		
+
 		end if;
-		
+
 	end process;
 
 end Behavioral;
-
